209cb73fc0a4ac6d316a5d39b398fc5cef7ca81b
JamesMokua <60739581+JamesMokua@users.noreply.github.com>
2021-02-03 17:25:46 +0300

    Update index.js

--- data/index.js
+++ data/index.js
@@ -3,92 +3,92 @@ export const sounds = [
 		id: 1,
 		title: 'Get Lucky',
 		artist: 'Daft Punk ft. Pharrell Williams, Nile Rodgers',
-		source: require('./sounds/getlucky.mp3'),
-		artwork: require('./artworks/getlucky.png')
+		source: require('./songs/getlucky.mp3'),
+		artwork: require('./artwork/getlucky.png')
 	},
 	{
 		id: 2,
 		title: 'OneRepublic',
 		artist: 'Counting Stars',
-		source: require('./sounds/countingstars.mp3'),
-		artwork: require('./artworks/countingstars.jpg')
+		source: require('./songs/countingstars.mp3'),
+		artwork: require('./artwork/countingstars.jpg')
 	},
 	{
 		id: 3,
 		title: 'Lights',
 		artist: 'Ellie Goulding',
-		source: require('./sounds/lights.mp3'),
-		artwork: require('./artworks/lights.jpg')
+		source: require('./songs/lights.mp3'),
+		artwork: require('./artwork/lights.jpg')
 	},
 	{
 		id: 4,
 		title: 'Grenade',
 		artist: 'Bruno Mars',
-		source: require('./sounds/grenade.mp3'),
-		artwork: require('./artworks/grenade.jpg')
+		source: require('./songs/grenade.mp3'),
+		artwork: require('./artwork/grenade.jpg')
 	},
 	{
 		id: 5,
 		title: 'Addicted To You',
 		artist: 'Avicii',
-		source: require('./sounds/addictedtoyou.mp3'),
-		artwork: require('./artworks/addictedtoyou.jpg')
+		source: require('./songs/addictedtoyou.mp3'),
+		artwork: require('./artwork/addictedtoyou.jpg')
 	},
 	{
 		id: 6,
 		title: 'Renegades',
 		artist: 'X Ambassadors',
-		source: require('./sounds/renegades.mp3'),
-		artwork: require('./artworks/renegades.jpg')
+		source: require('./songs/renegades.mp3'),
+		artwork: require('./artwork/renegades.jpg')
 	},
 	{
 		id: 7,
 		title: 'Riptide',
 		artist: 'Vance Joy',
-		source: require('./sounds/riptide.mp3'),
-		artwork: require('./artworks/riptide.jpg')
+		source: require('./songs/riptide.mp3'),
+		artwork: require('./artwork/riptide.jpg')
 	},
 	{
 		id: 8,
 		title: 'Dont Stop Me Now',
 		artist: 'Queen',
-		source: require('./sounds/dontstopmenow.mp3'),
-		artwork: require('./artworks/dontstopmenow.jpg')
+		source: require('./songs/dontstopmenow.mp3'),
+		artwork: require('./artwork/dontstopmenow.jpg')
 	},
 	{
 		id: 9,
 		title: 'Another Brick In The Wall',
 		artist: 'Pink Floyd',
-		source: require('./sounds/anotherbrickinthewall.mp3'),
-		artwork: require('./artworks/anotherbrickinthewall.jpg')
+		source: require('./songs/anotherbrickinthewall.mp3'),
+		artwork: require('./artwork/anotherbrickinthewall.jpg')
 	},
 	{
 		id: 10,
 		title: 'Piano Sonata No. 1 in F minor',
 		artist: 'Ludwig van Beethoven',
-		source: require('./sounds/pianosonata.mp3'),
-		artwork: require('./artworks/pianosonata.jpg')
+		source: require('./songs/pianosonata.mp3'),
+		artwork: require('./artwork/pianosonata.jpg')
 	},
 	{
 		id: 11,
 		title: 'Symphony No. 40 in G minor K.550',
 		artist: 'Wolfgang Amadeus Mozart',
-		source: require('./sounds/symphony.mp3'),
-		artwork: require('./artworks/symphony.jpg')
+		source: require('./songs/symphony.mp3'),
+		artwork: require('./artwork/symphony.jpg')
 	},
 	{
 		id: 12,
 		title: 'Violin Sonata No. 2 in A minor BWV.1003',
 		artist: 'Johann Sebastian Bach',
-		source: require('./sounds/violinsonata.mp3'),
-		artwork: require('./artworks/violinsonata.jpg')
+		source: require('./songs/violinsonata.mp3'),
+		artwork: require('./artwork/violinsonata.jpg')
 	},
 	{
 		id: 13,
 		title: 'Cello Sonata No. 3 in A Minor RV.43',
 		artist: 'Antonio Vivaldi',
-		source: require('./sounds/cellosonata.mp3'),
-		artwork: require('./artworks/cellosonata.jpg')
+		source: require('./songs/cellosonata.mp3'),
+		artwork: require('./artwork/cellosonata.jpg')
 	}
 ]
 
